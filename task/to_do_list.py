f9de23a1f0c49f55e02fbe500333b433e3da49b6
gomesleke <voyagerleke@gmail.com>
2026-01-07 23:41:35 -0300

    i add function to add task

--- task/to_do_list.py
+++ task/to_do_list.py
@@ -1,5 +1,7 @@
 import os
 
+task_storage= []
+
 def start():
     print('''\n| TO - DO LIST |''')
     print('\nthis is your to do list\n')
@@ -16,13 +18,35 @@ def invalid():
     input("Press to any keyboard: ")
     main()
 
+def add_function():
+    clean_screen()
+    print('| MODE: ADD |')
+    while True:
+
+        
+        name_task_input=input('Add your task (cls): ')
+        check_task=False
+
+        if name_task_input == 'cls':
+            break
+
+        task_sys={'status':check_task,'name':name_task_input}
+
+        task_storage.append(task_sys)
+
+        for task in task_storage:
+            status_print='[X]' if task['status'] else '[ ]'
+            print(f'{status_print} - {task['name']}')
+
+        
+
 def choice():
     user_choice=int(input('Write your choice: '))
 
     try:
         match user_choice:
             case 1:
-                print('vai corinthians')
+                add_function()
             
             case _:
                 invalid()

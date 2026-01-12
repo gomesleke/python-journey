5e9c662b5a07cc88caa4af8a5d4b01248e9ab6ef
gomesleke <voyagerleke@gmail.com>
2026-01-12 19:57:13 -0300

    resolução

--- ProjectEuler/001_Multiples_3_5.py
+++ ProjectEuler/001_Multiples_3_5.py
@@ -1,13 +1,11 @@
 i=0
-for x in range(1,10):
+for x in range(1,1000):
 
     if x%3==0:
         i+=x
-        print(x)
     elif x%5==0:
         i+=x
-        print(x)
     else:
         pass
 
-print(f'multiplos de 5 e 3 abaixo de 10: {i}')
+print(f'multiplos de 5 e 3 abaixo de 1000: {i}')

4836c1a21d89605351c10e5e80d93b3177b385bd
gomesleke <voyagerleke@gmail.com>
2026-01-12 20:06:45 -0300

    mostrando os impares

---
+++ ProjectEuler/000_problem_zero.py
@@ -0,0 +1,14 @@
+'''
+"Um número é um quadrado perfeito, ou um número quadrado, se ele for o quadrado de um inteiro positivo. Por exemplo, 25 é um número quadrado porque 5^2=5x5=25; ele também é um quadrado ímpar.
+
+Os primeiros 5 números quadrados são: 1, 4, 9, 16, 25, e a soma dos quadrados ímpares é 1+9+25=35.
+
+Entre os primeiros 514 mil números quadrados, qual é a soma de todos os quadrados ímpares?"
+'''
+
+for x in range(1,6):
+    square_x=x**2
+    if x%2 !=0:
+        print(x)
+
+

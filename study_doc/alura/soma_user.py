7408362bc154ffccf2ff497875c54bba642f91a1
gomesleke <voyagerleke@gmail.com>
2026-01-16 19:51:04 -0300

    mudei int->float

--- study_doc/alura/soma_user.py
+++ study_doc/alura/soma_user.py
@@ -1,6 +1,6 @@
 #some de dois números digitados pelo usuário
-primeiro_numero_user=int(input('Digite um número: '))
-segundo_numero_user=int(input('Digite um número: '))
+primeiro_numero_user=float(input('Digite um número: '))
+segundo_numero_user=float(input('Digite um número: '))
 
 soma=sum((primeiro_numero_user,segundo_numero_user))
 

68e5c131958923170699b95c384837b546fcc314
gomesleke <voyagerleke@gmail.com>
2026-01-16 19:49:13 -0300

    estudando back end me python na alura

---
+++ study_doc/alura/soma_user.py
@@ -0,0 +1,7 @@
+#some de dois números digitados pelo usuário
+primeiro_numero_user=int(input('Digite um número: '))
+segundo_numero_user=int(input('Digite um número: '))
+
+soma=sum((primeiro_numero_user,segundo_numero_user))
+
+print(f'A soma é {soma}')

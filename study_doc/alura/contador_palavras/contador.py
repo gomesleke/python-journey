a51633a3ade1296387e1eab5ef255b3e40c22f8b
gomesleke <voyagerleke@gmail.com>
2026-01-16 20:38:38 -0300

    sistema de remover caracteres, sistema de não contar palavras repetidas por um dicionário

--- study_doc/alura/contador_palavras/contador.py
+++ study_doc/alura/contador_palavras/contador.py
@@ -1,4 +1,20 @@
-def contar_palavras(texto):
+def clean(texto):
+    texto=texto.lower()
+    caracteres=',.;/|\!@#$%¨&*()[]{}?°ª=+'
+    for char in caracteres:
+        texto=texto.replace(char,"")
+    return texto
 
-    palavras=texto.split() 
-    return len(palavras)
+
+def contar_palavras(frase):
+    frase=clean(frase)
+    if not frase.strip(): #.strip() remove espaço --  mas essa parte siginifica se não houver nada valido, ele retorna nada
+        return {}
+    palavras=frase.split()
+    contagem={}
+
+    for palavra in palavras:
+        contagem[palavra]=contagem.get(palavra,0)+1 #sistema que impede contar a mesma palavra masi de uma vez
+
+
+    return contagem
--- study_doc/alura/contador_palavras/main.py
+++ study_doc/alura/contador_palavras/main.py
@@ -1,5 +1,8 @@
 from contador import contar_palavras
+from contador import clean
+import os
 
+os.system('cls')
 texto_user=str(input('Escreva o seu texto: '))
 quantidade=contar_palavras(texto_user)
 print(f'A quantidade de palavras é: {quantidade}')

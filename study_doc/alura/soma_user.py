407955f5ba0d06988d2128e99680f247f49bd387
gomesleke <voyagerleke@gmail.com>
2026-01-16 19:59:04 -0300

    otimização do código

--- study_doc/alura/soma_user.py
+++ study_doc/alura/soma_user.py
@@ -1,7 +1,19 @@
 #some de dois números digitados pelo usuário
-primeiro_numero_user=float(input('Digite um número: '))
-segundo_numero_user=float(input('Digite um número: '))
+import os
 
-soma=sum((primeiro_numero_user,segundo_numero_user))
+def problem():
+    try:
+        os.system('cls')
+        primeiro_numero_user=float(input('Digite um número: '))
+        segundo_numero_user=float(input('Digite um número: '))
 
-print(f'A soma é {soma}')
+        soma=sum((primeiro_numero_user,segundo_numero_user))
+        print(f'A soma é: {soma}')
+
+    except ValueError:
+        print('Ocorreu um erro, tente novamente!')
+        input('Aperte uma tecla do teclado: \n')
+        problem()
+
+problem()
+
